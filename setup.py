e2c68e3ea49b4469e7fb7a23db18d66e5b5ee0af
LevyvoNet <levyvonet@Elad-PC.localdomain>
2019-05-26 19:35:23 +0300

    done with PyPI template, tested on PyPITest

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="gym-mapf",
+    version="0.0.1",
+    author="LevyvoNet",
+    author_email="eladlevy2@gmail.com",
+    description="Multi-Agent Path Finding gym environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LevyvoNet/gym-mapf",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
