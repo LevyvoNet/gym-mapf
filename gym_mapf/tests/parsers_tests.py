40b9d4a00de8de750a1d41a0beb529687566886a
LevyvoNet <levyvonet@Elad-PC.localdomain>
2019-06-04 15:39:29 +0300

    added test for map parsing and made it pass

--- gym_mapf/tests/parsers_tests.py
+++ gym_mapf/tests/parsers_tests.py
@@ -2,11 +2,23 @@ import unittest
 import os
 
 from gym_mapf.envs import parse_scen_file
+from gym_mapf.utils.grid import MapfGrid, EmptyCell, ObstacleCell
 
 
+# TODO: test maps with obstacles.
 class ParsersTest(unittest.TestCase):
     def test_map_parser_empty_8_8(self):
-        pass
+        map_file_path = os.path.abspath(os.path.join(__file__, '../../maps/empty-8-8/empty-8-8.map'))
+        grid = MapfGrid(map_file_path)
+
+        self.assertTrue(grid[(0, 0)] is EmptyCell)
+        self.assertTrue(grid[(1, 1)] is EmptyCell)
+        self.assertTrue(grid[(0, 1)] is EmptyCell)
+        self.assertTrue(grid[(2, 1)] is EmptyCell)
+        self.assertTrue(grid[(7, 7)] is EmptyCell)
+
+        with self.assertRaises(IndexError):
+            grid[(8, 1)]
 
     def test_scen_parser_emtpy_8_8(self):
         scen_file_path = os.path.abspath(os.path.join(__file__, '../../maps/empty-8-8/empty-8-8-even-1.scen'))
--- gym_mapf/utils/grid.py
+++ gym_mapf/utils/grid.py
@@ -16,9 +16,14 @@ CHAR_TO_CELL = {
 class MapfGrid:
     def __init__(self, map_file):
         self._map = []
-        for line in open(map_file, 'r'):
-            new_line = [CHAR_TO_CELL[char]() for char in line]
-            self._map.append(new_line)
+        with open(map_file, 'r') as f:
+            lines = iter(f)
+            for _ in range(4):  # skip first 4 lines
+                next(lines)
+            for line in lines:
+                line = line.strip()
+                new_line = [CHAR_TO_CELL[char] for char in line]
+                self._map.append(new_line)
 
     def __getitem__(self, item):
         row, col = item
