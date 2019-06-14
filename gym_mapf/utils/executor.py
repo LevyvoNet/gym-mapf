bfe9cf2eaa640680f6f366d933f44633ab7d3a34
LevyvoNet <levyvonet@Elad-PC.localdomain>
2019-06-14 17:05:57 +0300

    stays if hit obstacle - now works

--- gym_mapf/utils/executor.py
+++ gym_mapf/utils/executor.py
@@ -1,5 +1,6 @@
 import copy
 from gym_mapf.utils.state import MapfState
+from gym_mapf.utils.grid import ObstacleCell
 
 UP = 'UP'
 RIGHT = 'RIGHT'
@@ -10,23 +11,38 @@ STAY = 'STAY'
 ACTIONS = [UP, RIGHT, DOWN, LEFT, STAY]
 
 
-def execute_up(loc, map_height, map_width):
+def stay_if_hit_obstacle(exec_func):
+    def new_exec_func(loc, map):
+        new_loc = exec_func(loc, map)
+        if map[new_loc] is ObstacleCell:
+            return loc
+
+        return new_loc
+
+    return new_exec_func
+
+
+@stay_if_hit_obstacle
+def execute_up(loc, map):
     return max(0, loc[0] - 1), loc[1]
 
 
-def execute_down(loc, map_height, map_width):
-    return min(map_height - 1, loc[0] + 1), loc[1]
+@stay_if_hit_obstacle
+def execute_down(loc, map):
+    return min(len(map) - 1, loc[0] + 1), loc[1]
 
 
-def execute_right(loc, map_height, map_width):
-    return loc[0], min(map_width - 1, loc[1] + 1)
+@stay_if_hit_obstacle
+def execute_right(loc, map):
+    return loc[0], min(len(map[0]) - 1, loc[1] + 1)
 
 
-def execute_left(loc, map_height, map_width):
+@stay_if_hit_obstacle
+def execute_left(loc, map):
     return loc[0], max(0, loc[1] - 1)
 
 
-def execute_stay(loc, *args):
+def execute_stay(loc, _):
     return loc
 
 
@@ -54,7 +70,6 @@ def execute_action(s, a):
     new_state = copy.deepcopy(s)
     for i, single_action in enumerate(a):
         exec_func = ACTION_TO_FUNC[single_action]
-        map_height, map_width = len(s.map), len(s.map[0])
-        new_state.agent_locations[i] = exec_func(new_state.agent_locations[i], map_height, map_width)
+        new_state.agent_locations[i] = exec_func(new_state.agent_locations[i], s.map)
 
     return new_state
