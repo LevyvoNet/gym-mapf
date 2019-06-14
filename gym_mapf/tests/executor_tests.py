c7d4dd017e8f711e605f1fe3e610fe4b4c6bd32e
LevyvoNet <levyvonet@Elad-PC.localdomain>
2019-06-14 16:02:58 +0300

    wrote a test for hitting an obstacle, not passing at the current

--- gym_mapf/tests/executor_tests.py
+++ gym_mapf/tests/executor_tests.py
@@ -7,7 +7,6 @@ from gym_mapf.utils.executor import UP, DOWN, LEFT, RIGHT, STAY, execute_action
 from gym_mapf.envs import parse_map_file
 
 
-
 # TODO: Make this test not depend on the file system.
 class ExecutorTest(unittest.TestCase):
     def test_moving_on_empty_grid(self):
@@ -31,8 +30,18 @@ class ExecutorTest(unittest.TestCase):
         new_state = execute_action(s, (LEFT, RIGHT))
         self.assertEqual(new_state.agent_locations, [(0, 0), (7, 7)])
 
-    # def test_against_obstacle_stays_in_place(self):
-    #     raise NotImplementedError()
+    def test_against_obstacle_stays_in_place(self):
+        grid = MapfGrid([
+            '..@..',
+            '..@..',
+            '.....',
+            '..@..',
+            '..@..'])
+
+        s = MapfState(grid, [(0, 1)])  # start near an obstacle.
+
+        new_state = execute_action(s, (RIGHT,))
+        self.assertEqual(new_state.agent_locations, [(0, 1)])  # The agent hits an obstacle and should stay in place.
 
     def test_stay_action(self):
         map_file_path = os.path.abspath(os.path.join(__file__, '../../maps/empty-8-8/empty-8-8.map'))
@@ -42,7 +51,6 @@ class ExecutorTest(unittest.TestCase):
 
         new_state = execute_action(s, (STAY, STAY))
         self.assertEqual(new_state.agent_locations, [(0, 0), (7, 7)])
-
 
 
 if __name__ == '__main__':
